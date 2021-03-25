6e7386ccc7abefdcf483b3edaa3c01c8400c9a02
Alex Marder <amarder@gmail.com>
2021-03-25 02:15:55 -0700

    readme

--- setup.py
+++ setup.py
@@ -5,16 +5,10 @@ setup(
     version='0.0.1',
     author='Alex Marder',
     # author_email='notlisted',
-    description="File opener for gzip, bzip2, or uncompressed files.",
-    url="https://github.com/alexmarder/traceutils",
+    description="File opener for gzip, bzip2, xz, or uncompressed files.",
+    url="https://github.com/alexmarder/file2",
     packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'rotate-scamper=rotate.rotate_scamper:main',
-            'rotate-targets=rotate.rotate_targets:main'
-        ],
-    },
     zip_safe=False,
     include_package_data=True,
-    python_requires='>3.7'
+    python_requires='>3.6'
 )
